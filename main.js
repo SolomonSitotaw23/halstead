7aa32181dde59c6d9f131d4178f5cc1bfcd42308
Solomon Sitotaw <50806926+SolomonSitotaw23@users.noreply.github.com>
2022-02-04 21:23:44 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let n1=document.getElementById("n1");
+let calculate=document.getElementById("submit");
+let inp =  document.getElementById("pl")
+
+calculate.addEventListener("click", calc);
+
+function calc() {
+    inp.innerHTML = "Program Length(N): 22";
+  }

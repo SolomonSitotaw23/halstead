bbb77c08020b1f948e7e01e38d7ae3e2c518ab5d
Solomon Sitotaw <50806926+SolomonSitotaw23@users.noreply.github.com>
2022-02-05 15:53:35 +0300

    beta 1

--- main.js
+++ main.js
@@ -7,37 +7,43 @@ let programVocabularyAns = document.getElementById("programVocabularyAns");
 let programVolumeAns = document.getElementById("programVolumeAns");
 let programDifficultyAns = document.getElementById("programDifficultyAns");
 let programLevelAns = document.getElementById("programLevelAns");
+let programEffortAns = document.getElementById("programEffortAns");
+let programmingTimeAns = document.getElementById("programmingTimeAns");
+
+let programBugsAns = document.getElementById("programBugsAns");
+
+
+
+
 
 
 
 let programLength, programVocabulary;
 
 calculate.addEventListener("click", () => {
-  let dOperator = document.getElementById("dn1").value; //n1
-  let tOperator = document.getElementById("tn1").value; //N1
-  let dOperands = document.getElementById("dn2").value; //n2
-  let tOperands = document.getElementById("tn2").value; //N2
+  let dOperator = document.getElementById("dn1").value; //n1 //! Getting Distinct  Operators
+  let tOperator = document.getElementById("tn1").value; //N1 //!   ""    total     Operators
+  let dOperands = document.getElementById("dn2").value; //n2 //!   ""    distinct  Operands
+  let tOperands = document.getElementById("tn2").value; //N2 //!   ""    total     Operands
 //!______________________________________Getters_____________________________________
   container.classList.add("right-panel-active");
-  programLength = parseInt(tOperator) + parseInt(tOperands);
-  programVocabulary = parseInt(dOperator) + parseInt(dOperands);
-  let programVolume = calculateVolume(programLength, programVocabulary);
-  let programDifficulty = calculateDifficulty(dOperator, tOperands, dOperands);
-  let programLevel = calculateLevel(programDifficulty);
-  let programEffort = calculateEffort(programDifficulty,programVolume);
-
-
-
+  programLength = parseInt(tOperator) + parseInt(tOperands);                      //!Length
+  programVocabulary = parseInt(dOperator) + parseInt(dOperands);                  //!Vocabulary
+  let programVolume = calculateVolume(programLength, programVocabulary);          //!Volume
+  let programDifficulty = calculateDifficulty(dOperator, tOperands, dOperands);   //!difficulty
+  let programLevel = calculateLevel(programDifficulty);                           //!Level
+  let programEffort = calculateEffort(programDifficulty,programVolume);           //!Effort
+  let programTime = calculateTime(programEffort);                                 //!Time
+  let programBugs = calculateBugs(programEffort);                                 //!Bugs
   // ! ___________________________Outputs______________________________________________
-  programLengthAns.placeholder ="Program Length = " + programLength;                //! length
-  programVocabularyAns.placeholder = "Program Vocabulary = " + programVocabulary;   //!Vocabulary
-  programVolumeAns.placeholder = "Program Volume = " + programVolume;               //!Volume
-  programDifficultyAns.placeholder = "Program Difficulty = " + programDifficulty;   //!difficulty
-  programLevelAns.placeholder = "Program Level = " + programLevel;                  //!Level
-
-
-
-
+  programLengthAns.placeholder ="Program Length = " + programLength;                   //! length
+  programVocabularyAns.placeholder = "Program Vocabulary = " + programVocabulary;      //!Vocabulary
+  programVolumeAns.placeholder = "Program Volume = " + programVolume;                  //!Volume
+  programDifficultyAns.placeholder = "Program Difficulty = " + programDifficulty;      //!difficulty
+  programLevelAns.placeholder = "Program Level = " + programLevel;                     //!Level
+  programEffortAns.placeholder = "Program Effort = " + programEffort;                  //!Effort
+  programmingTimeAns.placeholder = "Program Time = " + programTime;                    //!Time
+  programBugsAns.placeholder = "Program Bugs = " + programBugs;                    //!Time
 });
 
 
@@ -65,7 +71,7 @@ function calculateDifficulty(n1, N2, n2) {
 
 //! program Level calculator
 function calculateLevel(difficulty) {
-  difficulty = parseInt(difficulty);
+  difficulty = parseFloat(difficulty);
 
   let programLevel = (1 / difficulty);
 
@@ -74,15 +80,32 @@ function calculateLevel(difficulty) {
 
 //! program Effort calculator
 function calculateEffort(difficulty,volume) {
-  difficulty = parseInt(difficulty);
-  volume = parseInt(volume);
+  difficulty = parseFloat(difficulty);
+  volume = parseFloat(volume);
 
   let programEffort = (difficulty * volume);
 
   return programEffort;
 }
+//! program Time calculator
+function calculateTime(effort) {
+  effort = parseFloat(effort);
+  
 
+  let programTime = (effort/18);
 
+  return programTime;
+}
+
+//! program Bugs calculator
+function calculateBugs(effort) {
+  effort = parseFloat(effort);
+  effortsqu= effort*effort;
+
+  let programBugs = (Math.cbrt(effortsqu));
+
+  return programBugs;
+}
 
 goBack.addEventListener("click", () => {
   container.classList.remove("right-panel-active");
